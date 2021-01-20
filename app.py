d12b1c081d4ef3e0513d5f4bec6bdde2858b77c2
Alasia MEli <alasiameli@gmail.com>
2021-01-20 16:45:21 -0500

    added method documentation

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ import json
 
 from flask import Flask, jsonify, request, abort, Response, make_response
 
+# Create the Flask API
 app = Flask(__name__)
 
-
+# Define GET method to recover the information sent from the compliance_agent, for the host in which it was excecuted
+# It includes a handler exception in case that the file couldn't be created or opened.
 @app.route('/api/v1/compliance/system_info/<string:event_name>', methods=['GET'])
 def get_system_info_event(event_name):
     try:
@@ -15,7 +17,9 @@ def get_system_info_event(event_name):
     except Exception as e:
         return custom_error("Event not found", 404)
 
-
+# Define POST method waiting to receive a payload in .json format
+# The request body is saved as .json file named as server_ip_YYYY_MM_DD_H_M_S.json
+# It includes a handler exception in case that the file couldn't be created.
 @app.route('/api/v1/compliance/system_info', methods=['POST'])
 def create_system_info_event():
     jsonBody = request.json
@@ -32,12 +36,12 @@ def create_system_info_event():
     except Exception as e:
         return custom_error("File not created", 500)
 
-
+# Get the destination path to save the .json file
 def get_destination(path, file_name, extension):
     destination = path + file_name + extension
     return destination
 
-
+# Define the error in case that it needs to be used handling exceptions
 def custom_error(message, status_code):
     return make_response(jsonify({'status_code': status_code, 'message': message}), status_code)
 
